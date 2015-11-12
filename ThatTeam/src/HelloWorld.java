19b4c3d61eedb8f471569d9d696c292c71e84d2d
Dan <dangray@uw.edu>
2015-11-11 19:49:14 -0800

    Commit?

--- ThatTeam/src/HelloWorld.java
+++ ThatTeam/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Helloooooo. -Trish :)");
 		System.out.println("Mika is here too :)");
+		System.out.println("This is Dan");
 	}
 }
