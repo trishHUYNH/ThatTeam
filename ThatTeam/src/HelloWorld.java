4af6c92501fc4dc7af0435ca7b79878646b80d53
Brandon Thomas <brandondeanthomas@gmail.com>
2015-11-12 10:35:50 -0800

    veni, vidi, ego dormivi

--- ThatTeam/src/HelloWorld.java
+++ ThatTeam/src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Mika is here too :)");
 		System.out.println("This is Dan");
 		System.out.println("\"James waz here\" - James, 2015");
+		System.err.println("Red text liek a baws - Brandon");
 	}
 }
